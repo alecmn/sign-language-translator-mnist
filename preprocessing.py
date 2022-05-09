8cbf92d859de4c258e0d5eef7550346f8d105540
Alec Nonnemaker <a.m.nonnemaker@student.tudelft.nl>
2022-05-09 09:34:13 +0200

    Finished pre-processing (verification)

--- preprocessing.py
+++ preprocessing.py
@@ -1,31 +1,31 @@
-from torch.utils.data import Dataset 
+from torch.utils.data import Dataset
 from torch.autograd import Variable
 import torchvision.transforms as transforms
-import torch.nn as nn 
-import numpy as np 
-import torch 
+import torch.nn as nn
+import numpy as np
+import torch
+
+import csv
 
 
-import csv 
-
-class SignLanguageMNIST(Dataset): 
+class SignLanguageMNIST(Dataset):
     """
     Sign Language Letters classification dataset.
     Each sample is 1 x 1 x 28 x 28 
     """
 
-    @staticmethod 
-    def map_to_alphabet(): 
+    @staticmethod
+    def map_to_alphabet():
         # letters J and Z are exlcluded because they require motion 
         # so the dataset has labels 0-23 
         # this is transformed to alphabet labels 0-25 (26 letters)
         mapp = list(range(25))
         mapp.pop(9)
 
-        return mapp  
+        return mapp
 
-    @staticmethod 
-    def extract(path:str): 
+    @staticmethod
+    def extract(path: str):
         """
         Extracts labels and samples from the CSV file
         28 x 28 = 784 pixel values per sample 
@@ -58,15 +58,29 @@ class SignLanguageMNIST(Dataset):
 
     def __len__(self):
         return len(self._labels)
-    
+
     def __getitem__(self, idx):
         transform = transforms.Compose([
             transforms.ToPILImage(),
             transforms.RandomResizedCrop(28, scale=(0.8, 1.2)),
             transforms.ToTensor(),
             transforms.Normalize(mean=self._mean, std=self._std)])
-        
+
         return {
             'image': transform(self._samples[idx]).float(),
             'label': torch.from_numpy(self._labels[idx]).float()
         }
+
+
+def get_train_test_loader(batch_size=32):
+    trainset = SignLanguageMNIST('data/sign_mnist_train.csv')
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+    testset = SignLanguageMNIST('data/sign_mnist_test.csv')
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    return trainloader, testloader
+
+
+if __name__ == '__main__':
+    loader, _ = get_train_test_loader(2)
+    print(next(iter(loader)))
