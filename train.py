d9340f4e93a560699f714eb43ec7147c4c04b282
Sara Boby <sboby1720@gmail.com>
2022-05-22 15:25:00 +0200

    learning curve function in train.py

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
+from cProfile import label
+from turtle import color
 import torch
 from torch import nn, optim
 from torch.autograd import Variable
 import torch.nn.functional as F
 from torch.utils.data import Dataset
+
+import numpy as np 
+import matplotlib.pyplot as plt 
 
 from preprocessing import get_train_test_loader
 from networks import BaseNet, SmallNet, LargeNet
@@ -26,14 +31,58 @@ def main():
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
-        trainloader, _ = get_train_test_loader()
+        trainloader, testloader = get_train_test_loader()
+
+
+        train_losses = []
+        train_accs = []
+
+        test_losses = []
+        test_accs = []
+
         for epoch in range(20):
-            train(net, criterion, optimizer, trainloader, epoch)
-        torch.save(net.state_dict(), f"models/model_params_{name}2.pth")
+            train_loss, train_acc = train(net, criterion, optimizer, trainloader, epoch)
+
+            test_loss, test_acc = test(net, criterion, testloader)
+
+            train_losses.append(train_loss)
+            train_accs.append(train_accs)
+
+            test_losses.append(test_loss)
+            test_accs.append(test_acc)
+        
+        torch.save(net.state_dict(), f"models/model_params_{name}3.pth")
+
+        plot_learning_curves((train_losses, test_losses), (train_accs, test_accs))
+
+
+def plot_learning_curves(losses, accs, epochs):
+    fig, axes = plt.subplots(2)
+    plt.title('Learning curve')
+
+    
+    x = np.linspace(0, epochs, epochs)
+    
+    lc_train = axes[0].plot(x, losses[0], color='red')
+    lc_test = axes[0].plot(x, losses[1], color='red')
+    axes[0].set_xlabel('epoch')
+    axes[0].set_ylabel('loss')
+
+    lc_train = axes[1].plot(x, accs[0], color='red')
+    lc_test = axes[1].plot(x, accs[1], color='red')
+    axes[1].set_xlabel('epoch')
+    axes[1].set_ylabel('accuracy')
+
+    plt.show()
+
+
+
 
 
 def train(net, criterion, optimizer, trainloader, epoch):
     running_loss = 0.0
+    total = 0 
+    correct = 0 
     for i, data in enumerate(trainloader, 0):
         inputs = Variable(data['image'].float())
         labels = Variable(data['label'].long())
@@ -47,6 +96,40 @@ def train(net, criterion, optimizer, trainloader, epoch):
         running_loss += loss.item()
         if i % 100 == 0:
             print(f"[{epoch},{i}] loss: {running_loss / (i + 1)}")
+
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+        return running_loss/len(trainloader), 100*correct/total
+
+
+def test(net, criterion, test_loader):
+    running_loss = 0.0
+    total = 0 
+    correct = 0 
+
+    with torch.no_grad():   
+        for data in test_loader:
+            inputs, labels = data 
+
+            #forward pass 
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+
+        running_loss += loss.item()
+        
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    return running_loss/len(test_loader), 100*correct/total
+
+            
+
+
+
+
 
 
 if __name__ == '__main__':
